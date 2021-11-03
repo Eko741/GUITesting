dbee7365b545a77e9c7eea4be759682df304b151
Eko741PC <erik@lindaroger.com>
2021-11-03 21:17:17 +0100

    Add project files.

---
+++ GUITesting/renderer.cpp
@@ -0,0 +1,80 @@
+#include <math.h>
+void clearScreen() {
+	unsigned int* pixel = (unsigned int*)renderState.memory;
+	for (int i = 0; i < renderState.height; i++)
+		for (int j = 0; j < renderState.width; j++)
+			*pixel++ = 0;
+}
+
+inline int clamp(int a, int min, int max) {
+	const double t = a < min ? min : a;
+	return t > max ? max : t;
+}
+void drawRectF(unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned int color){
+	if (x > renderState.width || y > renderState.height) return;
+	if (x + width > renderState.width) width -= x + width - renderState.width;
+	if (y + height > renderState.height) height -= y + height - renderState.height;
+	
+	unsigned int* pixel = (unsigned int*)renderState.memory + x + y* renderState.width;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++)
+			*pixel++ = color;
+		pixel += renderState.width - width;
+	}
+}
+
+void drawRect(unsigned int x, unsigned int y, unsigned int width, unsigned int height, unsigned int color) {
+	if (x > renderState.width || y > renderState.height) return;
+	if (x + width > renderState.width) width -= x + width - renderState.width;
+	if (y + height > renderState.height) height -= y + height - renderState.height;
+
+	unsigned int* pixel = (unsigned int*)renderState.memory + x + y * renderState.width;
+	for (int i = 0; i < width; i++) *pixel++ = color;
+	for (int i = 0; i < height; i++) {
+		*pixel = color;
+		pixel += renderState.width;
+	}
+	for (int i = 0; i < width; i++) *pixel-- = color;
+	for (int i = 0; i < height; i++) {
+		*pixel = color;
+		pixel -= renderState.width;
+	}
+}
+
+void drawCircleF(unsigned int x, unsigned int y, int radius, unsigned int color) {
+	if (x - radius > renderState.width || y - radius > renderState.height) return;
+	int xOverlap = x + radius - renderState.width;
+	if (xOverlap < 0) xOverlap = 0;
+	else xOverlap++;
+	int yOverlap = y + radius - renderState.height;
+	if (yOverlap < 0) yOverlap = 0;
+	else yOverlap++;
+
+	unsigned int* pixel = (unsigned int*)renderState.memory + x - radius + (y - radius) * renderState.width;
+	for (int i = 0; i < radius * 2 + 1 - yOverlap; i++) {
+		for (int j = 0; j < radius * 2 + 1 - xOverlap; j++) {
+			if (sqrt(pow(i - radius, 2) + pow(j - radius, 2)) <= radius) *pixel = color;
+			pixel++;
+		}
+		pixel += renderState.width - 2*radius - 1 + xOverlap;
+	}
+}
+void drawCircle(unsigned int x, unsigned int y, int radius, unsigned int color) {
+	if (x - radius > renderState.width || y - radius > renderState.height) return;
+	int xOverlap = x + radius - renderState.width;
+	if (xOverlap < 0) xOverlap = 0;
+	else xOverlap++;
+	int yOverlap = y + radius - renderState.height;
+	if (yOverlap < 0) yOverlap = 0;
+	else yOverlap++;
+	
+	unsigned int* pixel = (unsigned int*)renderState.memory + x - radius + (y - radius) * renderState.width;
+	for (int i = 0; i < radius * 2  + 1 - yOverlap; i++) {
+		for (int j = 0; j < radius * 2 + 1 - xOverlap; j++) {
+			double d = sqrt(pow(i - radius, 2) + pow(j - radius, 2));
+			if (d >= radius - 1 && d <= radius + 1) *pixel = color;
+			pixel++;
+		}
+		pixel += renderState.width - 2 * radius - 1 + xOverlap;
+	}
+}
---
+++ GUITesting/win32_platform.cpp
@@ -0,0 +1,87 @@
+#include <windows.h>
+#include <cstdlib>
+using namespace std;
+bool running = true;
+
+struct RenderState {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmapInfo;
+} renderState; 
+
+#include "renderer.cpp"
+
+LRESULT CALLBACK window_callback(HWND   hwnd, UINT   uMsg, WPARAM wParam, LPARAM lParam) {
+	LRESULT result = 0;
+	switch (uMsg) {
+	case WM_CLOSE:
+	case WM_DESTROY: {
+		running = false;
+	} break;
+
+	case WM_SIZE: {
+
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		renderState.width = rect.right - rect.left;
+		renderState.height = rect.bottom - rect.top;
+
+		if (renderState.memory) VirtualFree(renderState.memory, 0, MEM_RELEASE);
+
+		int bufferSize = renderState.width * renderState.height * sizeof(unsigned int);
+		renderState.memory = VirtualAlloc(0, bufferSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+		renderState.bitmapInfo.bmiHeader.biSize = sizeof(renderState.bitmapInfo.bmiHeader);
+		renderState.bitmapInfo.bmiHeader.biWidth = renderState.width;
+		renderState.bitmapInfo.bmiHeader.biHeight = -renderState.height;
+		renderState.bitmapInfo.bmiHeader.biPlanes = 1;
+		renderState.bitmapInfo.bmiHeader.biBitCount = 32;
+		renderState.bitmapInfo.bmiHeader.biCompression = BI_RGB;
+	}
+
+	default: {
+		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	}
+	return result;
+}
+
+
+int  WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	// Create Window Class
+	WNDCLASS windowClass = {};
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpszClassName = "GUI";
+	windowClass.lpfnWndProc = window_callback;
+	
+	// Register Class
+	RegisterClass(&windowClass);
+	
+	// Craete Window
+	HWND window = CreateWindow(windowClass.lpszClassName, "GUI", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME  | WS_MAXIMIZEBOX | WS_VISIBLE , CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
+	// Game loop
+	int x = 200;
+	while (running) {
+		// Input
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		// Simulate
+		
+		clearScreen();
+		drawRect(300, 100, 75, 75, 0xffaaff);
+		drawRectF(100, 100, 100, 100, 0xffffff);
+		drawCircle(x, 600, 50, 0xaaffff);
+		drawCircleF(600, 600, 50, 0xaaffff);
+		
+		
+		x++;
+		// Render
+		StretchDIBits(hdc, 0, 0, renderState.width, renderState.height, 0, 0, renderState.width, renderState.height, renderState.memory, &renderState.bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+	}
+	
+}
